87254e7a9f8cb5ba01b2fa04db943c7636f66248
TomDeng <xianrendzw@hotmail.com>
2011-08-18 18:03:56 +0800

    Create LightFramework.Data.Test project

---
+++ LightFramework.Data.Test/DTO/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightFramework.Data.Test.DTO
+{
+    public class CategoryDTO
+    {
+
+    }
+}
