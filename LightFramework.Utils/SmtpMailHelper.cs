7f7277bc309378155f8c54852a87944cf2b898c0
xianrendzw <xianrendzw@hotmail.com>
2012-07-06 17:40:45 +0800

    update SmtpMailHelper.cs

--- LightFramework.Utils/SmtpMailHelper.cs
+++ LightFramework.Utils/SmtpMailHelper.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
+using System.IO;
 
 namespace LightFramework.Util
 {
@@ -13,6 +15,7 @@ namespace LightFramework.Util
         /// 发送邮件可带附件，System.Net.Mail命名空间
         /// </summary>
         /// <param name="host">邮件服务器地址</param>
+        /// <param name="port">邮件服务器端口</param>
         /// <param name="from">发件人邮箱地址</param>
         /// <param name="fromPassword">发件人邮箱密码</param>
         /// <param name="fromDisplayName">发件人名字</param>         
@@ -22,8 +25,8 @@ namespace LightFramework.Util
         /// <param name="body">邮件内容</param> 
         /// <param name="attachmentFiles">附件文件名集合</param>
         /// <returns>成功返回true，否则返回false</returns>
-        public static bool Send(string host, string from, string fromPassword, string fromDisplayName,
-            List<ToMailAddress> toMailAddress, string subject, string body,out string desc, params string[] attachmentFileNames)
+        public static bool Send(string host, int port, string from, string fromPassword, string fromDisplayName,
+            List<ToMailAddress> toMailAddress, string subject, string body, out string desc, params string[] attachmentFileNames)
         {
             if (toMailAddress == null ||
                 toMailAddress.Count == 0)
@@ -34,25 +37,25 @@ namespace LightFramework.Util
             try
             {
                 SmtpClient smtpClient = new SmtpClient(host);
-
-                smtpClient.Timeout = 60000;
-                smtpClient.UseDefaultCredentials = true;
+                smtpClient.Timeout = 60000;//获取或设置一个值，该值指定同步 Send 调用的超时时间。
+                smtpClient.UseDefaultCredentials = true;//获取或设置 Boolean 值，该值控制 DefaultCredentials 是否随请求一起发送。
                 smtpClient.Credentials = new NetworkCredential(from, fromPassword);
                 smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                //smtpClient.Port = 25;
+                smtpClient.Port = port;
                 //smtpClient.EnableSsl = true;
 
                 MailMessage message = new MailMessage();
-
-                message.SubjectEncoding = Encoding.UTF8;
-                message.BodyEncoding = Encoding.UTF8;
                 //message.From = new MailAddress(from, fromDisplayName, Encoding.UTF8); //.net4以前版本使用
-                message.From = new MailAddress(from);
+                message.From = new MailAddress(from, fromDisplayName);
                 //message.Sender = new MailAddress(from, fromDisplayName);
                 message.IsBodyHtml = true;
-                message.Subject = subject;
-                message.Body = body;
                 message.Priority = MailPriority.Normal;
+
+                message.SubjectEncoding = Encoding.UTF8;
+                message.Subject = subject;
+
+                message.BodyEncoding = Encoding.UTF8;
+                message.Body = body;
 
                 SetToMailAddress(toMailAddress, message);
                 SetAttachments(attachmentFileNames, message);
@@ -74,6 +77,7 @@ namespace LightFramework.Util
         /// 发送邮件可带附件，System.Net.Mail命名空间
         /// </summary>
         /// <param name="host">邮件服务器地址</param>
+        /// <param name="port">邮件服务器端口</param>
         /// <param name="from">发件人邮箱地址</param>
         /// <param name="fromPassword">发件人邮箱密码</param>
         /// <param name="fromDisplayName">发件人名字</param>         
@@ -83,13 +87,13 @@ namespace LightFramework.Util
         /// <param name="body">邮件内容</param> 
         /// <param name="attachmentFiles">附件文件名集合</param>
         /// <returns>成功返回true，否则返回false</returns>
-        public static bool Send(string host, string from, string fromPassword, string fromDisplayName,
+        public static bool Send(string host, int port, string from, string fromPassword, string fromDisplayName,
             string to, string toDisplayName, string subject, string body, out string desc, params string[] attachmentFileNames)
         {
             List<ToMailAddress> toMailAddress =
                 new List<ToMailAddress> { new ToMailAddress { Address = to, DisplayName = toDisplayName } };
 
-            return Send(host, from, fromPassword, fromDisplayName, toMailAddress, subject, body, out desc,attachmentFileNames);
+            return Send(host, port, from, fromPassword, fromDisplayName, toMailAddress, subject, body, out desc, attachmentFileNames);
         }
 
         private static void SetAttachments(string[] attachmentFileNames, MailMessage message)
